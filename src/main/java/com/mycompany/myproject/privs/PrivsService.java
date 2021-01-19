e5d25cbc9f73f8d8378087df373f3a4d2cac5d52
yogi45 <ogilestari45@gmail.com>
2021-01-19 09:34:41 +0700

    .

--- src/main/java/com/mycompany/myproject/privs/PrivsService.java
+++ src/main/java/com/mycompany/myproject/privs/PrivsService.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Service
 public class PrivsService {
 	
 	@Autowired
